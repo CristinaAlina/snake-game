fe100a29981cc295cc1899a04f05291990ebf5f3
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-08 23:28:36 +0200

    Created the Scoreboard class inheriting from the Turtle class, for keeping up the score and write it on the screen

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 13, "bold")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 280)
+        self.pencolor("white")
+
+    def increase_score(self):
+        self.score += 1
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score:  {self.score}", move=False, align=ALIGNMENT, font=FONT)
