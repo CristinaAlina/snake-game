37ee58f4d2c6ac52c6bd7451cf89db47f31d6ce5
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-08 21:07:47 +0200

    Detect collision with food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-# TODO: Create the food
-# TODO: Detect collision with food
+
 # TODO: Create a score board
 # TODO: Detect collision with wall
 # TODO: Detect collision with tail
@@ -8,6 +7,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -16,6 +16,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(fun=snake.up, key="Up")
 screen.onkey(fun=snake.down, key="Down")
@@ -26,7 +28,11 @@ game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # if the snake head is within 15 pixels of the food, or even closer, relocate the food location
+    if snake.snake_head.distance(food) < 15:
+        food.go_to_random_location()
+
 
 screen.exitonclick()
