2d32d5feed6e4c1f281522f4efe77beb0ac452cd
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-08 16:29:48 +0200

    Add listener for keys Up, Down, Right, Left, with interface updating

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 
-
-# TODO: Create snake food
+# TODO: Create the food
 # TODO: Detect collision with food
 # TODO: Create a score board
 # TODO: Detect collision with wall
 # TODO: Detect collision with tail
 
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
-
-
-def add_body_snake(body_snake):
-    new_square = Turtle("square")
-    new_square.pensize(20)
-    new_square.color("DarkGreen", "DarkGreen")
-    new_square.penup()
-    body_snake.append(new_square)
-    new_square.goto((-20 * body_snake.index(new_square)), 0)
-
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -25,21 +15,18 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake_body = []
-# create the starting shape of snake with 3 squares
-for index_square in range(3):
-    add_body_snake(snake_body)
+snake = Snake()
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
 
-# TODO: Move the snake
 game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-    for square_num in range(len(snake_body)-1, 0, -1):
-        new_x = snake_body[square_num-1].position()[0]
-        new_y = snake_body[square_num-1].position()[1]
-        snake_body[square_num].goto(new_x, new_y)
-    snake_body[0].forward(20)
 
+    snake.move()
 
 screen.exitonclick()
