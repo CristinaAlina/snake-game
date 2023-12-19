c157e8a1fc73636ccc3b4710ddc157e6394002eb
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-19 18:53:34 +0200

    Removed game over cycle of the game and added high score to keep the highest score visible on the screen

--- scoreboard.py
+++ scoreboard.py
@@ -7,18 +7,23 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.penup()
         self.goto(0, 280)
         self.pencolor("white")
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
+        self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score:  {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score:  {self.score}      High score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def end_of_game(self):
-        self.home()
-        self.write("Game over!", move=False, align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
